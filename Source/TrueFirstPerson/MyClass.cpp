38b2917fd5643904308140a01f810d0abc84cab7
Yunspace <yun@yunspace.com>
2017-02-23 00:53:02 +1100

    converted to CPP project and using VS2017

---
+++ Source/TrueFirstPerson/MyClass.cpp
@@ -0,0 +1,14 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "TrueFirstPerson.h"
+#include "MyClass.h"
+
+MyClass::MyClass()
+{
+
+}
+
+MyClass::~MyClass()
+{
+
+}
---
+++ Source/TrueFirstPerson/TrueFirstPerson.cpp
@@ -0,0 +1,5 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "TrueFirstPerson.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, TrueFirstPerson, "TrueFirstPerson" );
